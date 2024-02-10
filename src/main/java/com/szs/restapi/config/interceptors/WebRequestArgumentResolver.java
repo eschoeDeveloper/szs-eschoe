fb2a0c50a31bbc67b23c206e59de40d5509e3186
eschoeDeveloper <dev.uschoe@gmail.com>
2024-02-10 11:24:07 +0900

    [develop] jwt 설정 진행 중, 인코딩 허용 제한 설정(UTF-8)

---
+++ src/main/java/com/szs/restapi/config/interceptors/WebRequestArgumentResolver.java
@@ -0,0 +1,27 @@
+package com.szs.restapi.config.interceptors;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Log4j2
+@Component("webRequestArgumentResolver")
+@RequiredArgsConstructor
+public class WebRequestArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return false;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return null;
+    }
+
+}
