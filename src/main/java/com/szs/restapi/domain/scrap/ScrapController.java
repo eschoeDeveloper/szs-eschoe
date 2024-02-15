4267af4ffbb86e41ff62c1138868ddcd493b7f9c
eschoeDeveloper <dev.uschoe@gmail.com>
2024-02-15 22:17:02 +0900

    [develop] 스크래핑 API 호출 및 응답데이터 받기, Entity 및 Repository 작성중

--- src/main/java/com/szs/restapi/domain/scrap/ScrapController.java
+++ src/main/java/com/szs/restapi/domain/scrap/ScrapController.java
@@ -1,6 +1,7 @@
-package com.szs.restapi.domains.scrap;
+package com.szs.restapi.domain.scrap;
 
-import com.szs.restapi.domains.refund.RefundDTO;
+import com.szs.restapi.domain.refund.RefundDTO;
+import com.szs.restapi.globals.security.SzsUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -11,8 +12,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,8 +39,8 @@ public class ScrapController {
                     , content = @Content(schema = @Schema(implementation = RefundDTO.class)))
     })
     @PostMapping(value="/scrap", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> scrap (@RequestBody ScrapDTO scrapDTO) {
-        return ResponseEntity.ok(scrapService.hashCode());
+    public ResponseEntity<?> executeScrap (@RequestHeader("Authorization") String accessToken, @AuthenticationPrincipal SzsUserDetails userDetails) throws Exception {
+        return ResponseEntity.ok(scrapService.execute(accessToken, userDetails));
     }
 
 }
