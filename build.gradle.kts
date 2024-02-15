af7a7618f4b966b512f319ebb6828f85c9edab7d
eschoeDeveloper <dev.uschoe@gmail.com>
2024-02-15 22:09:33 +0900

    [develop] org.json 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.2.2"
+	id("org.springframework.boot") version "3.1.8"
 	id("io.spring.dependency-management") version "1.1.4"
 }
 
@@ -29,17 +29,19 @@ repositories {
 // annotationProcessor : 컴파일 단계에서 Annotation에 정의된 일렬의 프로세스를 동작하게 하는 것
 // testImplementation : 테스트 단계에서 컴파일 + 런타임이며, 빌드 결과물에도 포함된다. 테스트 코드 레벨에서 참조 가능
 dependencies {
-	implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.4")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	// implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.4")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.4")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+	implementation("org.json:json:20240205")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
 	compileOnly("org.projectlombok:lombok")
-	runtimeOnly("com.h2database:h2")
+	implementation("com.h2database:h2")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
